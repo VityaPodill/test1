25ae034315bb5426a03a1ef544cf9d0d86a54a9c
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-09-24 20:42:54 +0300

    Add times method

--- Launcher.java
+++ Launcher.java
@@ -3,13 +3,17 @@ public class Launcher {
         int a = 5, b = 3;
         System.out.println(add(a, b));
         minus(a, b);
+        System.out.println(times(a, b));
     }
-
     private static int add(int a, int b) {
         return a + b;
     }
 
     private static void minus(int a, int b) {
         System.out.println(a - b);
+    }
+
+    private static int times(int a, int b) {
+        return a * b;
     }
 }

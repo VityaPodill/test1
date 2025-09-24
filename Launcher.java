e65a9342638592437d052e7836d44b6fff5c7e33
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-09-24 20:34:12 +0300

    Add minus-void Class

--- Launcher.java
+++ Launcher.java
@@ -2,10 +2,15 @@ public class Launcher {
     public static void main(String[] args) {
         int a = 5, b = 3;
         System.out.println(add(a, b));
+        minus(a, b);
     }
 
     private static int add(int a, int b) {
         return a + b;
+    }
+
+    private static void minus(int a, int b) {
+        System.out.println(a - b);
     }
 
 }

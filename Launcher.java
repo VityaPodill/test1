5c020596509ed84e1f8c9216ad26cb48750ea9c3
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-09-25 20:19:36 +0300

    Add sub (a/b) method

--- Launcher.java
+++ Launcher.java
@@ -1,9 +1,10 @@
 public class Launcher {
     public static void main(String[] args) {
         int a = 5, b = 3;
-        System.out.println(add(a, b));
-        minus(a, b);
-        System.out.println(times(a, b));
+//        System.out.println(add(a, b));
+//        minus(a, b);
+//        System.out.println(times(a, b));
+        System.out.println(sub(a, b));
     }
     private static int add(int a, int b) {
         return a + b;
@@ -15,5 +16,9 @@ public class Launcher {
 
     private static int times(int a, int b) {
         return a * b;
+    }
+
+    private static int sub(int a, int b) {
+        return a / b;
     }
 }

a6866f2628354f91a7bd8c115f9f6ad0e1b1e6ff
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-09-25 20:33:09 +0300

    Remove comments from 3 rows

--- Launcher.java
+++ Launcher.java
@@ -1,9 +1,9 @@
 public class Launcher {
     public static void main(String[] args) {
         int a = 5, b = 3;
-//        System.out.println(add(a, b));
-//        minus(a, b);
-//        System.out.println(times(a, b));
+        System.out.println(add(a, b));
+        minus(a, b);
+        System.out.println(times(a, b));
         System.out.println(sub(a, b));
     }
     private static int add(int a, int b) {

9fb3e6b2183dec557f0ded64fe86680b373ef1fc
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-10-04 21:02:48 +0300

    Add SearchMethod IDEA

--- Launcher.java
+++ Launcher.java
@@ -1,7 +1,13 @@
 public class Launcher {
     public static void main(String[] args) {
-        int a = 15, b = 13;
-
+        String c = "qwerty uio asdfg hjkl", d = "o a";
+        int lc = c.length(), ld = d.length();
+        for (int i = 0; i <= lc - ld; i++) {
+            if (c.substring(i, i + ld).equals(d)) {
+                System.out.println("YES 111");
+                break;
+            } else if (i == lc - ld) System.out.println("N0");
+        }
 
     }
 }

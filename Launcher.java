bf281c17aaa4e111832935a2552a3085a44bef3e
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-10-04 19:19:28 +0300

    Delete all & prepare for coding

--- Launcher.java
+++ Launcher.java
@@ -1,39 +1,6 @@
 public class Launcher {
     public static void main(String[] args) {
         int a = 5, b = 3;
-        System.out.println(add(a, b));
-        minus(a, b);
-        System.out.println(times(a, b));
-        System.out.println(sub(a, b));
-        System.out.println(max(a, b));
 
-        System.out.println(a);
-        System.out.println(isBigger(a, b));
-        //----------
-        //----+++++++------
-        //----+++++++------
-    }
-    private static int add(int a, int b) {
-        return a + b;
-    }
-
-    private static void minus(int a, int b) {
-        System.out.println(a - b);
-    }
-
-    private static int times(int a, int b) {
-        return a * b;
-    }
-
-    private static int sub(int a, int b) {
-        return a / b;
-    }
-
-    private static int max(int a, int b) {
-        return Math.max(a, b);
-    }
-
-    private static boolean isBigger(int a, int b) {
-        return a > b;
     }
 }

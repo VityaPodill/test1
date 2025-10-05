0a6cc10eaa344f51463e80db84c5ef5b72e262e4
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-10-05 14:34:17 +0300

    Delete not necessary IDEA

--- Launcher.java
+++ Launcher.java
@@ -6,8 +6,6 @@ public class Launcher {
             if (c.substring(i, i + ld).equals(d)) {
                 System.out.println("YES 111");
                 break;
-                //=======
-                //---------
             } else if (i == lc - ld) System.out.println("N0");
         }
     }
